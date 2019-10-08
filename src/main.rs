0cfbb8a87e936e96aa0d47d5f9f97b2904910b8a
Mathias Iversen <work@mathiasiversen.com>
2019-10-08 23:40:41 +0200

    started to work with yaml instead of json

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,62 @@
-use serde_json::Value;
 use std::fs::{ File };
 use std::io::prelude::*;
 use structopt::StructOpt;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use serde_yaml::Value;
 
 mod cli;
 use cli::{
-    Cli,
-    Validator
+    Cli
 };
-
-mod file_types {
-    pub mod css;
-    pub mod scss;
-    pub mod js;
-}
 
 type Result<T> = std::result::Result<T, std::boxed::Box<dyn std::error::Error>>;
 
-const STR_DONT_EDIT: &str = r"DON'T EDIT THIS FILE - IT'S GENERATED";
+// const STR_DONT_EDIT: &str = r"DON'T EDIT THIS FILE - IT'S GENERATED";
+
+type Constants = HashMap<String, HashMap<Value, Value>>;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+struct Inputfile {
+    output_files: HashMap<String, Outputfile>,
+    constants: Constants,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+struct Outputfile {
+    path: String,
+    constants: Vec<String>,
+}
 
 fn main() -> Result<()> {
-    let opt = Cli::from_args().is_valid();
+    let opt = Cli::from_args();
 
     let mut file = File::open(&opt.input)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let root: Value = serde_json::from_str(&contents)?;
-    let root = root.as_object().expect("The file provided is not a json");
-    
-    for (index, (key, value)) in root.iter().enumerate() {
-        if opt.dir_scss.is_some() {
-            file_types::scss::write_to_file(key, value, &opt.dir_scss.clone().unwrap())?;
-        }
-        if opt.dir_css.is_some() {
-            let last_value = index == root.len() - 1;
-            file_types::css::write_to_file(value, &opt.dir_css.clone().unwrap(), last_value)?;
-        }
-        if opt.dir_js.is_some() {
-            file_types::js::write_to_file(key, value, &opt.dir_js.clone().unwrap())?;
+    let serialized: Inputfile = serde_yaml::from_str(&contents).unwrap();
+
+    for (file_ending, output_file) in serialized.output_files.iter() {
+        match file_ending.as_str() {
+            "js" => create_js_file(output_file, &serialized.constants),
+            "scss" => create_scss_file(output_file, &serialized.constants),
+            _ => unimplemented!(),
         }
     }
+
     Ok(())
+}
+
+fn create_js_file(output_file: &Outputfile, constants: &Constants) {
+    for constant_group in output_file.constants.iter() {
+        // Create file format according to the file type
+        dbg!(constant_group, &constants[constant_group]);
+    }
+}
+fn create_scss_file(output_file: &Outputfile, constants: &Constants) {
+    for constant_group in output_file.constants.iter() {
+        // Create file format according to the file type
+        dbg!(constant_group, &constants[constant_group]);
+    }
 }
