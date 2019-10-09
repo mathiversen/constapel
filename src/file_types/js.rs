10728a3a2372fc988f71aa4dfe4200703d6f689d
Mathias Iversen <mathias.iversen@danielwellington.com>
2019-10-09 10:00:34 +0200

    implemented the core logic and with modules for each file type

--- src/file_types/js.rs
+++ src/file_types/js.rs
@@ -1,16 +1,23 @@
-use serde_json::Value;
-use std::path::PathBuf;
-use std::fs::{ File };
-use std::io::prelude::*;
-use crate::{ STR_DONT_EDIT, Result };
+// use serde_json::Value;
+// use std::path::PathBuf;
+// use std::fs::{ File };
+// use std::io::prelude::*;
+// use crate::{ STR_DONT_EDIT, Result };
 
-pub fn write_to_file(name: &str, value: &Value, path: &PathBuf) -> Result<()> {
-    let mut file_css = File::create(format!("{}/{}.js", &path.to_string_lossy(), &name))?;
-    file_css.write_all(format!("// {}\n\n", STR_DONT_EDIT).as_bytes())?;
-    file_css.write_all("export default {\n".as_bytes())?;
-    for v in value.as_object().expect("Failed to parse value") {
-        file_css.write_all(format!("    {}: {},\n", v.0, v.1).as_bytes())?;
-    }
-    file_css.write_all("}\n".as_bytes())?;
+// pub fn write_to_file(name: &str, value: &Value, path: &PathBuf) -> Result<()> {
+//     let mut file_css = File::create(format!("{}/{}.js", &path.to_string_lossy(), &name))?;
+//     file_css.write_all(format!("// {}\n\n", STR_DONT_EDIT).as_bytes())?;
+//     file_css.write_all("export default {\n".as_bytes())?;
+//     for v in value.as_object().expect("Failed to parse value") {
+//         file_css.write_all(format!("    {}: {},\n", v.0, v.1).as_bytes())?;
+//     }
+//     file_css.write_all("}\n".as_bytes())?;
+//     Ok(())
+// }
+
+use crate::{Constants, STR_DONT_EDIT, Result};
+
+pub fn create(output_file: &str, constants: &Constants) -> Result<()>{
+    dbg!(output_file, constants);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,24 @@ use cli::{
     Cli
 };
 
+mod file_types {
+    pub mod js;
+    pub mod scss;
+}
+
 type Result<T> = std::result::Result<T, std::boxed::Box<dyn std::error::Error>>;
-
-// const STR_DONT_EDIT: &str = r"DON'T EDIT THIS FILE - IT'S GENERATED";
-
 type Constants = HashMap<String, HashMap<Value, Value>>;
 
+const STR_DONT_EDIT: &str = r"DON'T EDIT THIS FILE - IT'S GENERATED";
+
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
-struct Inputfile {
+pub struct Inputfile {
     output_files: HashMap<String, Outputfile>,
     constants: Constants,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
-struct Outputfile {
+pub struct Outputfile {
     path: String,
     constants: Vec<String>,
 }
@@ -38,25 +42,20 @@ fn main() -> Result<()> {
     let serialized: Inputfile = serde_yaml::from_str(&contents).unwrap();
 
     for (file_ending, output_file) in serialized.output_files.iter() {
+        let relevant_constants: Constants = serialized.constants.iter().fold(HashMap::new(), |mut acc, (key, value)| {
+            if output_file.constants.contains(&key) {
+                acc.insert(key.clone(), value.clone());
+                acc
+            } else {
+                acc
+            }
+        });
         match file_ending.as_str() {
-            "js" => create_js_file(output_file, &serialized.constants),
-            "scss" => create_scss_file(output_file, &serialized.constants),
+            "js" => file_types::js::create(&output_file.path, &relevant_constants)?,
+            "scss" => file_types::scss::create(&output_file.path, &relevant_constants)?,
             _ => unimplemented!(),
         }
     }
 
     Ok(())
-}
-
-fn create_js_file(output_file: &Outputfile, constants: &Constants) {
-    for constant_group in output_file.constants.iter() {
-        // Create file format according to the file type
-        dbg!(constant_group, &constants[constant_group]);
-    }
-}
-fn create_scss_file(output_file: &Outputfile, constants: &Constants) {
-    for constant_group in output_file.constants.iter() {
-        // Create file format according to the file type
-        dbg!(constant_group, &constants[constant_group]);
-    }
 }
