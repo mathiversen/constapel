57a53feb2a08d515dce32e9f7b25e6d8fc3294cd
Mathias Iversen <mathias.iversen@danielwellington.com>
2019-12-16 16:32:21 +0100

    moved tests to separate folder + removed unindent

--- src/file_creator.rs
+++ src/file_creator.rs
@@ -62,29 +62,3 @@ impl FileCreator {
         })
     }
 }
-
-mod tests {
-    #![allow(unused_imports)]
-    use super::*;
-    use unindent::unindent;
-
-    #[test]
-    fn parse_yaml_string() {
-        let yaml = r#"
-          output_files:
-            js:
-              path: '.'
-              constants:
-                - colors
-          constants:
-            colors:
-              white: '#ffffff'
-        "#;
-        let c = FileCreator::from_yaml(yaml.to_string()).unwrap();
-        assert_eq!(&c.output_files["js"].path, ".");
-        assert_eq!(
-            &c.constants["colors"][&serde_yaml::Value::String("white".to_string())],
-            "#ffffff"
-        );
-    }
-}
